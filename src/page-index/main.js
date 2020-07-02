804b30f4ebdf29c1fe9cc6b8db756edc9fb969d9
Ilya Petruhin <59193488+IlyaPetruhin@users.noreply.github.com>
2020-07-02 15:15:10 +0300

    changedtoscss

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -2,7 +2,10 @@ import { Bar } from '../js/bar.es6'
 const { Foo } = require('../js/foo.cjs')
 require('normalize.css/normalize.css')
 require('../css/main.css')
-require('./page.css')
+// require('./page.css')
+require('../page-index/header.scss')
+require('../page-index/pages.scss')
+require('../page-index/footer.scss')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-index')
