cdd879552af495c3b307c63d93cf7f16f635e417
Ilya Petruhin <59193488+IlyaPetruhin@users.noreply.github.com>
2020-07-21 16:01:29 +0300

    changed

--- src/page-about/main.js
+++ src/page-about/main.js
@@ -1,9 +1,9 @@
 require('normalize.css/normalize.css')
 require('../css/main.css')
-require('./page.css')
-const img = require('../img/unicorn.jpg')
+require('./page.scss')
+require('../page-index/header.scss')
+require('../page-index/footer.scss')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-about')
-  console.log('Image through require()', img)
 })
